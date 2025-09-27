4bbd755f560a9843d38aec2e59105be044a18304
hhcczz <101077489+hhcczz@users.noreply.github.com>
2025-09-27 20:20:08 +0900

    Update GiftTripPages00.jsx

--- JSX/GiftTripPages00.jsx
+++ JSX/GiftTripPages00.jsx
@@ -1,35 +1,31 @@
-import { useNavigate } from "react-router-dom";
-import "./CSS/GiftTripPages00.css";
+import "../CSS/GiftTripPages00.css";
+import "../CSS/Common.css";
 
-export default function PageOne() {
-  const navigate = useNavigate();
+export default function GiftTripPages00() {
 
   return (
-    <div className="page">
-      {/* 상단 헤더 (로고 중앙, 로그인 우상단) */}
-      <header className="header">
-        <h1 className="logo">Gift Trip</h1>
-        <button className="loginBtn">로그인</button>
+    <div className="CommonPage">
+      {/* 최상단 영역 */}
+      <header className="CommonHeader">
+        {/* 중앙 상단: 타이틀 */}
+        <h1 className="CommonLogo CommonLogo_Middle">Gift Trip</h1>
+        {/* 우측 상단: 로그인 */}
+        <button className="CommonLoginBtn">로그인</button>
       </header>
 
       {/* 메인 */}
-      <main className="main">
+      <main className="Page00_Main">
         {/* 질문 + 인풋은 붙여두기 */}
-        <div className="questionGroup">
-          <p className="question">어디로 여행을 떠나실 계획이신가요?</p>
-          <input className="input" placeholder="여행지를 입력해보세요." />
+        <div className="Page00_QuestionGroup">
+          <p className="Page00_Question">어디로 여행을 떠나실 계획이신가요?</p>
+          <input className="Page00_Input" placeholder="여행지를 입력해보세요." />
         </div>
 
         {/* 하단 쪽에 분리된 액션 영역 */}
-        <div className="actionArea">
-          <button className="actionBtn">여행지가 정해지지 않았어요.</button>
+        <div className="Page00_ActionArea">
+          <button className="Page00_ActionBtn">여행지가 정해지지 않았어요.</button>
         </div>
       </main>
-
-      {/* 오른쪽 하단 네비 버튼 (page2로 이동) */}
-      <button className="navBtn" onClick={() => navigate("/page2")}>
-        ➡ 다음 페이지
-      </button>
     </div>
   );
 }
